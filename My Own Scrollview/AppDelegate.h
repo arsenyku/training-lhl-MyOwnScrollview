787f0ba2404c525b60a420937622a810c9274532
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-07 10:03:12 -0700

    Initial Commit

---
+++ My Own Scrollview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Own Scrollview
+//
+//  Created by asu on 2015-09-07.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My Own Scrollview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My Own Scrollview
+//
+//  Created by asu on 2015-09-07.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
